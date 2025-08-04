5c5aaaeddf84f3dd1a50c89a44595582743c0394
Heathcliff <heathcliff@heathcliff.eu>
2025-08-04 17:28:47 +0200

    Use packit to create a RPM in copr
    Ship the package via copr repos by creating a RPM with packit.
    
    Closes: #78
    
    Signed-off-by: Heathcliff <heathcliff@heathcliff.eu>

--- pkg/version/version_test.go
+++ pkg/version/version_test.go
@@ -11,6 +11,11 @@ import (
 )
 
 func TestVersion(t *testing.T) {
+	oldGitCommit := gitCommit
+	defer func() { gitCommit = oldGitCommit }()
+
+	gitCommit = "1234567890abcdef"
+
 	result := Version()
 
 	lines := strings.Split(result, "\n")
@@ -19,14 +24,32 @@ func TestVersion(t *testing.T) {
 
 	buildinfo, _ := debug.ReadBuildInfo()
 
-	require.Equal(t, 5, len(lines), "Should have enough lines")
+	require.Len(t, lines, 5, "Should have enough lines")
 	assert.Contains(lines[0], Name)
 	assert.Contains(lines[1], buildinfo.Main.Version)
 
 	commit := strings.Split(lines[2], ":")
-	assert.NotEmpty(strings.TrimSpace(commit[1]))
+	assert.Equal("1234567", strings.TrimSpace(commit[1]), "commit hash should be truncated")
 
 	assert.Contains(lines[3], runtime.Version())
 
 	assert.Equal("", lines[4], "Should have trailing newline")
+}
+
+func TestInitGitCommit(t *testing.T) {
+	oldGitCommit := gitCommit
+	defer func() { gitCommit = oldGitCommit }()
+	assert := assert.New(t)
+
+	gitCommit = "1234567890abcdef"
+	initGitCommit()
+	assert.Equal("1234567890abcdef", gitCommit, "gitCommit should not be changed")
+
+	gitCommit = "$Format:%H$"
+	initGitCommit()
+	assert.NotEqual("$Format:%H$", gitCommit, "gitCommit should be changed")
+
+	gitCommit = ""
+	initGitCommit()
+	assert.Equal("Unknown", gitCommit, "gitCommit should be Unknown")
 }
