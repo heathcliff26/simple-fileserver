c180e3053c15480d2bf3ac492b5b13a97b989443
Heathcliff <heathcliff@heathcliff.eu>
2026-05-06 21:03:44 +0200

    CI: Use goreleaser to build release artifacts
    Instead of a matrix build job, use goreleaser instead.
    
    Signed-off-by: Heathcliff <heathcliff@heathcliff.eu>

--- hack/clean.sh
+++ hack/clean.sh
@@ -4,7 +4,7 @@ set -e
 
 base_dir="$(dirname "${BASH_SOURCE[0]}" | xargs realpath)/.."
 
-folders=("bin" "coverprofiles" "tmp" "x86_64" "aarch64")
+folders=("bin" "dist" "coverprofiles" "tmp" "x86_64" "aarch64")
 files=("coverprofile.out")
 
 for folder in "${folders[@]}"; do
